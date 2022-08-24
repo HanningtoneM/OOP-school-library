ea5db6cd6405a2955a9d657862ce65b259d44327
HanningtoneM <hanningtonepompyy@gmail.com>
2022-08-24 15:51:21 +0300

    Add relationship between classroom and student

--- student.rb
+++ student.rb
@@ -1,12 +1,18 @@
 require './person'
 
 class Student < Person
-  def initialize(id, name, age, classroom = 'Unknown', parent_permission: true)
+  attr_reader :classroom
+
+  def initialize(id, name, age, parent_permission: true)
     super(id, name, age, parent_permission: parent_permission)
-    @classroom = classroom
   end
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def add_classroom(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 end
