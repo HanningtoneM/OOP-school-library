e47694e1280a71652830e667d82dd40f686ad352
HanningtoneM <hanningtonepompyy@gmail.com>
2022-08-25 10:03:30 +0300

    Add list of all

--- app.rb
+++ app.rb
@@ -10,3 +10,29 @@ class App
     @id = 0
   end
 
+
+  def run
+    puts 'Welcome to School Library App!'
+    display_options
+
+    task_list = {
+      '1' => method(:list_all_books),
+      '2' => method(:list_all_people),
+      '3' => method(:create_person),
+      '4' => method(:create_book),
+      '5' => method(:create_rental),
+      '6' => method(:list_rentals)
+    }
+
+    task = gets.chomp
+    if task.to_i.positive? && task.to_i < 7
+      task_list[task].call
+    elsif task == '7'
+      puts 'Thank you for using this app!'
+    else
+      puts 'Please select a valid number'
+      run
+    end
+  end
+
+
