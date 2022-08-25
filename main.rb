ec0595bd3d889fa09ccd5d76978ba5e13effb66f
HanningtoneM <hanningtonepompyy@gmail.com>
2022-08-25 18:11:12 +0300

    made changes in main

--- main.rb
+++ main.rb
@@ -1,54 +1,57 @@
-require './person'
-require './rental'
-require './book'
-require './app'
+require_relative 'app'
 
-class App
-  def self.menu
-    puts 'Welcome to the School Library App!'
-    puts "\n"
-    puts 'Please choose an option by entering a number: '
-
-    choices = ['1 - List all book',
-               '2 - List all People',
-               '3 - Create a person',
-               '4 - Create a book',
-               '5 - Create a rental',
-               '6 - List all rentals for a given person id',
-               '7 - Exit']
-
-    choices.each { |choice| puts choice.to_s }
-
-    Integer(gets.chomp)
+class Main
+  def initialize
+    @app = App.new
   end
 
-  run = Library.new
+  def display_list
+    puts "
+    Please choose an option by entering a number:
+    1- List all books.
+    2- List all people.
+    3- Create a person (teacher or student, not a plain Person).
+    4- Create a book.
+    5- Create a rental.
+    6- List all rentals for a given person id.
+    7- Exit"
+  end
 
-  loop do
-    case menu
+  def execute_option(option)
+    case option
     when 1
-      run.list_books
+      @app.list_all_books
     when 2
-      run.list_people
+      @app.list_all_people
     when 3
-      run.create_person
+      @app.create_person
     when 4
-      run.create_book
+      @app.create_book
     when 5
-      run.create_rental
+      @app.create_rental
     when 6
-      run.list_rentals
-    when 7
-      puts 'Thank you for using the app!'
-      exit
+      @app.display_rentals_by_person_id
     else
-      puts 'Choose a number between 1 to 7'
+      puts 'Not a valid option'
     end
   end
+
+  def choose_option
+    loop do
+      display_list
+      option = gets.chomp.to_i
+      break if option == 7
+
+      execute_option(option)
+    end
+  end
+
+  def main
+    puts 'Welcome to School library app!'
+    choose_option
+    puts 'Thank you for using School library app!'
+  end
 end
 
-def main
-  App.new
-end
-
-main
+main = Main.new
+main.main
